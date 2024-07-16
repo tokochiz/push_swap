56c1b032e6dabec97e320be877bbb6cb229425a3
tokochiz <tokohana.1003@gmail.com>
2024-07-16 20:21:04 +0900

    add : 外殻を作成　関数名のみ

--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -6,7 +6,7 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 21:49:24 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2024/07/16 20:03:28 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2024/07/16 20:20:36 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,34 @@ void	move_a_to_b(t_ps *ps)
 	}
 }
 
+void	optimize_and_move_b_to_a(t_ps *ps)
+{
+	calculate_move_costs(ps);
+	calculate_optimal_target(ps);
+	set_best_move_method(ps);
+	ececute_move_method(ps);
+	optimize_stack_a(ps);
+}
 
 void	sort_stack(t_ps *ps)
 {
-
+	if (ps->a->size <= 5)
+	{
+		sort_small(ps);
+		return ;
+	}
 	// AからBに移動させる
 	move_a_to_b(ps);
 	print_stack(ps->a, 'A');
 	print_stack(ps->b, 'B');
-
 	update_min_max_b(ps);
 	printf("stack b min:%d max:%d\n", ps->min_b, ps->max_b);
-	// Bスタックの最小最大を探す、入れ替える、一番上もしくは一番したからpushする関数
-	// Aスタックに積んだ時に、ルールが保たれているか
-	// stackBの最大と最小を特定する
+	while (ps->b->size > 0)
+	{
+		optimize_and_move_b_to_a(ps);
+		adjust_stack_b(ps);
+		update_min_max_b(ps);
+		printf("stack b min:%d max:%d\n", ps->min_b, ps->max_b);
+	}
+	optimize_final_stack_a(ps);
 }
