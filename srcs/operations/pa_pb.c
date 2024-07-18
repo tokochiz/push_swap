7e5a9a1619b2986a875c55913754c6ab859d3042
tokochiz <tokohana.1003@gmail.com>
2024-07-18 20:38:23 +0900

    add : pa, pb

---
+++ srcs/operations/pa_pb.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pa_pb.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 19:53:59 by  ctokoyod         #+#    #+#             */
+/*   Updated: 2024/07/18 20:37:05 by  ctokoyod        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	pa(t_ps *ps)
+{
+	t_list	*first;
+
+	if (ps->b->size < 1)
+		put_error_and_exit(ERR_OP);
+	first = ps->b->top;
+	ps->b->top = first->next;
+	ps->b->size--;
+	first->next = ps->a->top;
+	ps->a->top = first;
+	ps->a->size++;
+	(*ps->operation_count)++;
+}
+void	pb(t_ps *ps)
+{
+	t_list	*first;
+
+	if (ps->a->size < 1)
+		put_error_and_exit(ERR_OP);
+	first = ps->a->top;
+	ps->a->top = first->next;
+	ps->a->size--;
+	first->next = ps->b->top;
+	ps->b->top = first;
+	ps->b->size++;
+	(*ps->operation_count)++;
+}
