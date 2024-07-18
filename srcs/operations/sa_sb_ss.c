b8c46b59bd8b5e346e24ce6b18cb63f730c3f13c
tokochiz <tokohana.1003@gmail.com>
2024-07-18 20:38:12 +0900

    add : ss operations

---
+++ srcs/operations/sa_sb_ss.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sa_sb_ss.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 19:52:54 by  ctokoyod         #+#    #+#             */
+/*   Updated: 2024/07/18 20:10:19 by  ctokoyod        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	stack_swap(t_list **top)
+{
+	t_list		*first;
+	t_list		*second;
+	intptr_t	tmp;
+
+	if (*top == NULL || (*top)->next == NULL)
+		put_error_and_exit(ERR_OP);
+	first = *top;
+	second = first->next;
+	tmp = (intptr_t)first->content;
+	first->content = second->content;
+	second->content = (void *)(intptr_t)tmp;
+}
+
+void	sa(t_ps *ps)
+{
+	if (ps->a->size < 2)
+		put_error_and_exit(ERR_OP);
+	stack_swap(&ps->a->top);
+	(*ps->operation_count)++;
+}
+
+void	sb(t_ps *ps)
+{
+	if (ps->b->size < 2)
+		put_error_and_exit(ERR_OP);
+	stack_swap(&ps->b->top);
+	(*ps->operation_count)++;
+}
+
+void	ss(t_ps *ps)
+{
+	bool	op;
+
+	op = false;
+	if (ps->a->size >= 2 && ps->b->size >= 2)
+	{
+		stack_swap(&ps->a->top);
+		stack_swap(&ps->b->top);
+		op = true;
+	}
+	if (op)
+		(*ps->operation_count)++;
+}
