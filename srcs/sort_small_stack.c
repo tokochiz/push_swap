56d15527e1dfb8215a1b97ecae57ea85c7f80bce
tokochiz <tokohana.1003@gmail.com>
2024-08-10 21:50:11 +0900

    add : 5以下のとき用sotr

---
+++ srcs/sort_small_stack.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_mini.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctokoyod <ctokoyod@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-08-10 11:56:12 by ctokoyod          #+#    #+#             */
+/*   Updated: 2024-08-10 11:56:12 by ctokoyod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+// sa: スタックAの上位2要素を交換
+void	sort_two(t_ps *ps)
+{
+	if ((int)(intptr_t)ps->a->top->content > (int)(intptr_t)ps->a->top->next->content)
+		sa(ps);
+}
+
+void	sort_three(t_ps *ps)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = (int)(intptr_t)ps->a->top->content;
+	b = (int)(intptr_t)ps->a->top->next->content;
+	c = (int)(intptr_t)ps->a->top->next->next->content;
+	if (a > b && b > c)
+	{
+		sa(ps);
+		rra(ps);
+	}
+	else if (a > b && b < c && c < a)
+		ra(ps); // ra: スタックAを上に1つシフト（一番上が一番下に）
+	else if (a < b && b > c && c < a)
+		rra(ps);
+	else if (a < b && b > c && c < a)
+	{
+		sa(ps);
+		ra(ps);
+	}
+	else if (a < c && b < c && c < a)
+		sa(ps);
+}
+
+void	sort_four(t_ps *ps)
+{
+	int	min_pos;
+
+	min_pos = find_min_position(ps->a);
+	// 最小値を一番上に持ってくる
+	if (min_pos == 1)
+		sa(ps);
+	else if (min_pos == 2)
+	{
+		ra(ps);
+		ra(ps);
+	}
+	else if (min_pos == 3)
+		rra(ps);
+	pb(ps);
+	sort_three(ps);
+	pa(ps);
+}
+
+void	sort_five(t_ps *ps)
+{
+	int	min_pos;
+
+	min_pos = find_min_position(ps->a);
+	// 最小値を一番上に持ってくる
+	if (min_pos == 1)
+		sa(ps);
+	else if (min_pos == 2)
+	{
+		ra(ps);
+		ra(ps);
+	}
+	else if (min_pos == 3)
+	{
+		rra(ps);
+		rra(ps);
+	}
+	else if (min_pos == 4)
+		rra(ps);
+	pb(ps);
+	sort_three(ps);
+	pa(ps);
+}
+
+void	sort_small_stack(t_ps *ps)
+{
+	int	a_size;
+
+	a_size = ps->a->size;
+	if (a_size == 2)
+		sort_two(ps);
+	else if (a_size == 3)
+		sort_three(ps);
+	else if (a_size == 4)
+		sort_four(ps);
+	else if (a_size == 5)
+		sort_five(ps);
+}
--- srcs/sort_stack.c
+++ srcs/sort_stack.c
@@ -12,48 +12,38 @@
 
 #include "../includes/push_swap.h"
 
+
 // 中央値を求める理由　動作ごとに回数を加算できているか　一旦、単純にA→Bの関数として使う
 void	move_a_to_b(t_ps *ps)
 {
-	int	median;
-	int	elm_to_move;
-	int	moved;
+	// int	median;
+	// int	elm_to_move;
+	// int	moved;
 
-	if (ps == NULL || ps->a == NULL || ps->a->top == NULL)
-		put_error_and_exit(ERR_STACK);
-	median = find_median(ps->a);
-	printf("Debug: Allocating memory for values array4 %d\n", median);
-	elm_to_move = ps->a->size / 2;
-	moved = 0;
-	while (ps->a->size > 3 && moved < elm_to_move)
-	{
-		if ((int)(intptr_t)ps->a->top->content < median)
-		{
-			pb(ps);
-			moved++;
-		}
-		else
-		{
-			ra(ps);
-		}
-	}
-	while (ps->a->size > 0)
+	// if (ps == NULL || ps->a == NULL || ps->a->top == NULL)
+	// 	put_error_and_exit(ERR_STACK);
+	// median = find_median(ps->a);
+	// printf("Debug: Allocating memory for values array4 %d\n", median);
+	// elm_to_move = ps->a->size / 2;
+	// moved = 0;
+	// while (ps->a->size > 5 && moved < elm_to_move)
+	// {
+	// 	if ((int)(intptr_t)ps->a->top->content < median)
+	// 	{
+	// 		pb(ps);
+	// 		moved++;
+	// 	}
+	// 	else
+	// 		ra(ps);
+	// }
+	while (ps->a->size > 5 && ps->a->size > 0)
 	{
 		pb(ps);
 	}
 	printf("Debug: Allocating memory for values array5\n");
-	// sort_three(ps->a);
+	sort_small_stack(ps);
 }
 
-// optimize 最適化する
-// void	optimize_and_move_b_to_a(t_ps *ps)
-// {
-// 	calculate_move_costs(ps);
-// 	calculate_optimal_target(ps);
-// 	set_best_move_method(ps);
-// 	execute_move_method(ps);
-// 	optimize_stack_a(ps);
-// }
 
 void	sort_stack(t_ps *ps)
 {
@@ -67,12 +57,11 @@ void	sort_stack(t_ps *ps)
 	printf_stack(ps->a, 'A');
 	printf_stack(ps->b, 'B');
 	update_min_max_b(ps);
-	printf("stack b min:%d max:%d\n", ps->min_b, ps->max_b);
 	while (ps->b->size > 0)
 		{
 		optimize_and_move_b_to_a(ps);
 		update_min_max_b(ps);
-		printf("stack b min:%d max:%d\n", ps->min_b, ps->max_b);
+		printf("2stack b min:%d max:%d\n", ps->min_b, ps->max_b);
 	 }
 	// 	optimize_final_stack_a(ps);
 }
